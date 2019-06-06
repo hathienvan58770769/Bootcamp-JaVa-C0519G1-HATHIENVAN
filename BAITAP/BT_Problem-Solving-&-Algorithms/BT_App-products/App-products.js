f1e2a1b0893572d32f447b20ed9ed83602859f0b
Hà Thiên Vân <hathienvan@MacBook-Pro-cua-Ha.local>
2019-06-06 10:55:40 +0700

    update

--- BAITAP/BT_Problem-Solving-&-Algorithms/BT_App-products/App-products.js
+++ BAITAP/BT_Problem-Solving-&-Algorithms/BT_App-products/App-products.js
@@ -1,3 +1,9 @@
-function addProducts(){
-    
+function add(){
+    this.product = document.getElementById('products');
+    this.listProducts = ['Samsung Galaxy', 'Nokia', 'Xiaomi Redmi', 'Apple iPhone', 'Oppo', 'Asus', 'Sky'];
+
+    this.count = function (data) {
+        let counter = document.getElementById('count');
+        let name = 
+    }
 }
